290656f49295dbd40873a8e3e37d3b025c354b12
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-10-30 17:51:36 -0300

    Implementação da página de Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
+import './styles.css';
 
 // import { Container } from './styles';
 
-export default function Login() {
-  return <div />;
+export default function Login({ history }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const response = await api.post('/users/auth', {
+      email,
+      password
+    });
+    console.log(response.data);
+  }
+
+  return (
+    <>
+      <h1>MONITONLINE | LOGIN</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">e-mail *</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          onChange={event => setEmail(event.target.value)}
+        />
+        <label htmlFor="password">senha *</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={event => setPassword(event.target.value)}
+        />
+        <p>
+          ainda não tem uma conta?{' '}
+          <Link to={'/register'} className="register-link">
+            cadastre-se já!
+          </Link>
+        </p>
+
+        <button type="submit">fazer login</button>
+      </form>
+    </>
+  );
 }
