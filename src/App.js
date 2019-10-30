65cd80ee7c27de2f6e709d5075328b8623cf7b7f
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-10-30 10:22:35 -0300

    Adição do campo email que estava faltando

--- src/App.js
+++ src/App.js
@@ -1,22 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import api from './services/api';
+
 function App() {
+  const [name, setName] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [course, setCourse] = useState(null);
+  const [is_tutor, setTutor] = useState(false);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      await api.post('/users', {
+        name,
+        email,
+        cpf,
+        password,
+        course,
+        is_tutor
+      });
+    } catch (err) {
+      console.log(err.response);
+    }
+  }
+
+  function handleRadio(value) {
+    if (value && typeof value === 'string') {
+      if (value.toLowerCase() === 'true') return true;
+      if (value.toLowerCase() === 'false') return false;
+    }
+    return value;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">NOME COMPLETO *</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Seu belo nome"
+          onChange={event => setName(event.target.value)}
+        />
+        <label htmlFor="cpf">CPF *</label>
+        <input
+          type="text"
+          name="cpf"
+          id="cpf"
+          placeholder="Seu CPF"
+          onChange={event => setCpf(event.target.value)}
+        />
+        <label htmlFor="email">E-MAIL *</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Seu melhor e-mail"
+          onChange={event => setEmail(event.target.value)}
+        />
+        <label htmlFor="password">SENHA *</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={event => setPassword(event.target.value)}
+        />
+        <label htmlFor="course">CURSO *</label>
+        <select
+          name="course"
+          id="course"
+          onChange={event => setCourse(parseInt(event.target.value))}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="0">Eletromecânica</option>
+          <option value="1">Tecnologia da Informação</option>
+        </select>
+        <label htmlFor="is_tutor">DESEJA SER MONITOR? *</label>
+        <input
+          type="radio"
+          name="is_tutor"
+          value="true"
+          onChange={event =>
+            setTutor(handleRadio(event.target.value))
+          }
+        />
+        Sim
+        <input
+          type="radio"
+          name="is_tutor"
+          value="false"
+          onChange={event =>
+            setTutor(handleRadio(event.target.value))
+          }
+        />
+        Não
+        <button type="submit">Cadastrar</button>
+      </form>
     </div>
   );
 }
