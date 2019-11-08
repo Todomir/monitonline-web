0e2ff6cf06f45e74245ad476567e1fd638e9c3d6
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-11-08 18:36:39 -0200

    Implementação de renders diferentes caso o aluno for monitor

--- src/pages/UserPage/index.js
+++ src/pages/UserPage/index.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 import { getToken } from '../../services/auth';
 
+import { MdDateRange, MdBook, MdQueryBuilder } from 'react-icons/md';
+
 import './styles.css';
 
 export default function UserPage() {
@@ -22,17 +24,22 @@ export default function UserPage() {
   if (user.is_tutor) {
     return (
       <>
-        <h1>MONITONLINE </h1>
-        <strong>
-          <h5>{user.name}</h5>
-        </strong>
+        <h1>MONITONLINE | PERFIL</h1>
 
         <div className="content-container">
           <div className="profile">
             <h2>Opções do monitor</h2>
-            <label>checar atendimentos</label>
-            <label>editar assuntos</label>
-            <label>adicionar horário</label>
+            <h5>{user.name}</h5>
+
+            <label>
+              <MdDateRange /> checar atendimentos
+            </label>
+            <label>
+              <MdBook /> editar assuntos
+            </label>
+            <label>
+              <MdQueryBuilder /> adicionar horário
+            </label>
           </div>
         </div>
       </>
@@ -41,9 +48,25 @@ export default function UserPage() {
     return (
       <>
         <h1>MONITONLINE </h1>
-        <strong>
-          <p>{user.name}</p>
-        </strong>
+        <div className="content-container">
+          <div className="profile">
+            <h2>Opções do aluno</h2>
+            <h5>{user.name}</h5>
+
+            <label>
+              <MdDateRange /> marcar atendimento
+              <p>clique aqui para procurar um monitor agora mesmo!</p>
+            </label>
+
+            <label>
+              <MdDateRange /> meus atendimentos
+              <p>
+                clique aqui para ver o status dos atendimentos
+                marcados
+              </p>
+            </label>
+          </div>
+        </div>
       </>
     );
   }
