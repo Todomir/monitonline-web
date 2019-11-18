767cc0a1c5d127558b9f976389f99023b3a86bb6
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-11-18 16:16:55 -0200

    Criação dos styled-components: FormLabel,   TextInput,   Button,   Title e   StyledLink

---
+++ src/components/styled-components/styles.js
@@ -0,0 +1,71 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Title = styled.h1`
+  font-size: 50px;
+  font-weight: 900px;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const TextInput = styled.input`
+  border: 1px solid rgb(235, 235, 235);
+  border-radius: 3px;
+  height: 45px;
+  padding: 10px;
+`;
+
+export const FormLabel = styled.label`
+  font-weight: 700;
+  margin-top: 30px;
+  margin-bottom: 4px;
+  font-size: 16px;
+`;
+
+export const Button = styled.button`
+  margin-top: 40px;
+  border: 0;
+  border-radius: 3px;
+  width: 100%;
+  height: 45px;
+  padding: 0 20px;
+  font-size: 16px;
+  font-weight: 900;
+  color: #fff;
+  background: #2575fc;
+  cursor: pointer;
+  box-shadow: 3px 3px 4px 0px rgba(0, 0, 0, 0.29);
+  -webkit-transition: all 500ms ease;
+  -moz-transition: all 500ms ease;
+  -ms-transition: all 500ms ease;
+  -o-transition: all 500ms ease;
+  transition: all 500ms ease;
+
+  &:hover {
+    background: #6a11cb;
+  }
+
+  &:active {
+    background: #2575f2;
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #2575fc;
+  cursor: pointer;
+  -webkit-transition: all 500ms ease;
+  -moz-transition: all 500ms ease;
+  -ms-transition: all 500ms ease;
+  -o-transition: all 500ms ease;
+  transition: all 500ms ease;
+
+  &:hover {
+    padding: 5px;
+    margin: 0 5px;
+    color: #fff;
+    font-weight: 700;
+    background: #2575fc;
+    border-radius: 3px;
+  }
+`;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 import { login } from '../../services/auth';
 import './styles.css';
+
+import {
+  FormLabel,
+  TextInput,
+  Button,
+  Title,
+  StyledLink
+} from '../../components/styled-components/styles';
 
 // import { Container } from './styles';
 
@@ -23,18 +30,18 @@ export default function Login({ history }) {
 
   return (
     <>
-      <h1>MONITONLINE | LOGIN</h1>
+      <Title>MONITONLINE | LOGIN</Title>
 
       <form onSubmit={handleSubmit}>
-        <label htmlFor="email">e-mail *</label>
-        <input
+        <FormLabel>e-mail *</FormLabel>
+        <TextInput
           type="email"
           name="email"
           id="email"
           onChange={event => setEmail(event.target.value)}
         />
-        <label htmlFor="password">senha *</label>
-        <input
+        <FormLabel>senha *</FormLabel>
+        <TextInput
           type="password"
           name="password"
           id="password"
@@ -42,14 +49,10 @@ export default function Login({ history }) {
         />
         <p>
           ainda não tem uma conta?{' '}
-          <Link to={'/register'} className="register-link">
-            cadastre-se já!
-          </Link>
+          <StyledLink to={'/register'}>cadastre-se já!</StyledLink>
         </p>
 
-        <button className="btn" type="submit">
-          fazer login
-        </button>
+        <Button type="submit">fazer login</Button>
       </form>
     </>
   );
