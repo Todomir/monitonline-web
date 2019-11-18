8bb92ce264d4286a120b685caf6fc5ff152fd0c2
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-11-18 19:45:07 -0200

    Substituição das tags HTML em SearchTutor e adição de um ToggleContainer

--- src/pages/SearchTutor/index.js
+++ src/pages/SearchTutor/index.js
@@ -5,9 +5,18 @@ import { MdSearch, MdAssignment } from 'react-icons/md';
 import api from '../../services/api';
 
 import './styles.css';
+import {
+  CardContainer,
+  CardContent,
+  FormLabel,
+  TextInput,
+  SmallLink
+} from '../../components/styled-components/styles';
+import ToggleContainer from '../../components/ToggleContainer';
 
 export default function SearchTutor({ history }) {
   const [subject_matter_description, setSMDescription] = useState('');
+  const [toggle, setToggle] = useState(false);
   const [tutors, setTutors] = useState([]);
 
   async function handleSubmit(event) {
@@ -24,6 +33,10 @@ export default function SearchTutor({ history }) {
     }
   }
 
+  function handleToggle() {
+    setToggle(!toggle);
+  }
+
   async function handleAssistanceClick(tutorId, subjectMatter) {
     const subjectMatterId = subjectMatter.id;
     localStorage.setItem('tutor_id', tutorId);
@@ -36,16 +49,16 @@ export default function SearchTutor({ history }) {
     <>
       <h1>MONITONLINE | TUTORIA</h1>
 
-      <div className="content-container">
-        <div className="profile">
+      <CardContainer>
+        <CardContent>
           <h2>Pesquisar tutores</h2>
 
           <form onSubmit={handleSubmit}>
-            <label htmlFor="subject_matter_description">
+            <FormLabel htmlFor="subject_matter_description">
               digite o assunto que deseja tutoria *
-            </label>
+            </FormLabel>
             <div className="search-input">
-              <input
+              <TextInput
                 type="text"
                 name="subject_matter_description"
                 placeholder="Trigonometria"
@@ -54,40 +67,46 @@ export default function SearchTutor({ history }) {
                   setSMDescription(event.target.value)
                 }
               />
-              <button type="submit">
+              <button onClick={handleToggle} type="submit">
                 <MdSearch />
               </button>
             </div>
           </form>
 
-          <label className="tutor-label">
-            {tutors.map(tutor => (
-              <>
-                <h4 key={tutor.name}>{tutor.name}</h4>
+          <ToggleContainer toggle={toggle}>
+            <CardContainer>
+              <CardContent>
+                {tutors.map(tutor => (
+                  <>
+                    <FormLabel key={tutor.name}>
+                      {tutor.name}
+                    </FormLabel>
 
-                <h5
-                  key={tutor.id}
-                  onClick={() => {
-                    handleAssistanceClick(
-                      tutor.id,
-                      tutor.subjectMatters.find(subjectMatter => {
-                        if (
-                          subjectMatter.subject_matter_description ===
-                          subject_matter_description
-                        )
-                          return subjectMatter;
-                      })
-                    );
-                  }}
-                >
-                  <MdAssignment key={tutor.id} /> ver horários
-                  disponíveis
-                </h5>
-              </>
-            ))}
-          </label>
-        </div>
-      </div>
+                    <SmallLink
+                      key={tutor.id}
+                      onClick={() => {
+                        handleAssistanceClick(
+                          tutor.id,
+                          tutor.subjectMatters.find(subjectMatter => {
+                            if (
+                              subjectMatter.subject_matter_description ===
+                              subject_matter_description
+                            )
+                              return subjectMatter;
+                          })
+                        );
+                      }}
+                    >
+                      <MdAssignment key={tutor.id} /> ver horários
+                      disponíveis
+                    </SmallLink>
+                  </>
+                ))}
+              </CardContent>
+            </CardContainer>
+          </ToggleContainer>
+        </CardContent>
+      </CardContainer>
     </>
   );
 }
