1d3f6b1d13b625172641ae4ef0a82617a17f4a1d
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-11-23 12:48:37 -0200

    Switch para mostrar o status do atendimento

--- src/components/Assistances/index.js
+++ src/components/Assistances/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import dateFormat from 'dateformat';
+import { SmallLink } from '../styled-components/styles';
 
 export default function Assistances({ assistances }) {
+  const status = id => {
+    switch (id) {
+      case 1:
+        return 'Marcado';
+        break;
+      case 2:
+        return 'Realizado';
+        break;
+      case 3:
+        return 'Cancelado pelo aluno';
+        break;
+      case 4:
+        return 'Cancelado pelo monitor';
+        break;
+      default:
+        return '404';
+    }
+  };
+
   return assistances.map(assistance => (
     <>
       <ul>
@@ -14,6 +34,10 @@ export default function Assistances({ assistances }) {
         De {dateFormat(assistance.schedule.schedule_start, 'dd-mm-yyyy HH:MM')} à{' '}
         {dateFormat(assistance.schedule.schedule_end, 'dd-mm-yyyy HH:MM')}
       </ul>
+      <ul>
+        <strong>Status: </strong> {status(assistance.status_id)}
+      </ul>
+      {}
       <br />
     </>
   ));
