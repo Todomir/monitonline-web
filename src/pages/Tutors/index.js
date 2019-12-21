63ec82f9772f6ac12ca8ac897e65aa4b4d9608c6
Abner Luis Rodrigues <abnerluis1001@gmail.com>
2019-12-21 09:02:04 -0300

    Criação da página de Tutors

---
+++ src/pages/Tutors/index.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { MdSearch, MdAssignment } from 'react-icons/md';
+
+import './styles.css';
+
+import Modal from '../../components/Modal';
+import {
+  CardContainer,
+  CardContent,
+  FormLabel,
+  TextInput,
+  SmallLink,
+  SubTitle,
+  Button,
+  Box
+} from '../../components/styled-components/styles';
+import api from '../../services/api';
+
+export default function Tutors() {
+  const [tutors, setTutors] = useState([]);
+  const [smDescription, setSmDescription] = useState('');
+
+  useEffect(() => {
+    async function getTutors() {
+      try {
+        const response = await api.post('/users/fetchUsersByDescription', {
+          subject_matter_description: smDescription
+        });
+        setTutors(response.data);
+      } catch (err) {
+        console.log(err.response);
+      }
+      getTutors();
+    }
+  }, [tutors]);
+
+  return (
+    <Box>
+      <SubTitle>
+        MONITORES ENSINANDO{' '}
+        <strong style={{ color: '#b276ff' }}>{smDescription.toUpperCase()}</strong>
+      </SubTitle>
+      {tutors.map(tutor => (
+        <>
+          <FormLabel key={tutor.name}>{tutor.name}</FormLabel>
+          <MdAssignment key={tutor.id} /> ver horários disponíveis
+        </>
+      ))}
+    </Box>
+  );
+}
